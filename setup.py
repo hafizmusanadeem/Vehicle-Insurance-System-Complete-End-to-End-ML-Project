881bc2884313d1be8f5aa88eeb6bdb2b719b22a6
hafizmusanadeem <musanad04@gmail.com>
2026-01-03 00:32:16 +0500

    Project Structure and Logger Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Musa Nadeem',
+    author_email='musanad04@gmail.com',
+    packages= find_packages()
+)
---
+++ src/logger/logger.py
@@ -0,0 +1,40 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+from from_root import from_root
+from datetime import datetime
+
+LOG_DIR = 'logs'
+LOG_FILE = f'{datetime.now().strftime('%m_%d_%Y+%H_%M_%S')}.log'
+MAX_LOG_SIZE = 5 * 1024 * 1024
+BACKUP_COUNT = 3
+
+def configure_logger():
+    log_dir_path = os.path.join(from_root(), LOG_DIR)
+    os.makedirs(log_dir_path, exist_ok=True)
+
+    # File Path
+    log_file_path = os.path.join(log_dir_path, LOG_FILE)
+    
+    # Logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    if logger.handlers:
+        return
+    
+    formatter = logging.Formatter(
+        "[ %(asctime)s %(name)s = %(levelname)s - %(message)s]"
+    )
+
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_COUNT)
+
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
